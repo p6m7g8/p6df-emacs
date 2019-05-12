622e3716b758ac739fd933c054fd79a3e329e967
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-05-11 21:25:22 -0400

    catch up homebrew

--- init.zsh
+++ init.zsh
@@ -1,18 +1,18 @@
 p6df::modules::emacs::version() { echo "0.0.1" }
-p6df::modules::emacs::deps()    { 
+p6df::modules::emacs::deps()    {
 	ModuleDeps=(p6m7g8/p6emacs)
 }
 
 p6df::modules::emacs::external::git() {
-  
+
   git clone https://github.com/magnars/.emacs.d
   git clone https://github.com/hlissner/doom-emacs
 }
 
 p6df::modules::emacs::external::brew() {
 
-  brew install emacs --with-cocoa --with-librsvg --with-mailutils --with-modules
-  brew install mu --with-emacs
+  brew install emacs-head --with-cocoa --with-imagemagick --with-mailutils
+  brew install mu
   brew install offlineimap
   brew install w3m
 }
